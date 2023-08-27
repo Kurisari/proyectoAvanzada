7979f36af4606c7b83d432ce8d053fec5e55924f
Cristian Aragon <crisarag8.sal@gmail.com>
2023-08-26 21:20:30 -0600

    Función menú agregada

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+    Integrantes
+    ? Cristian Aragón Salazar
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int menu(){
+    int eleccion;
+    cout << "Elige el tipo de numeros a usar:" << endl;
+    cout << "1. Consecutivos" << endl;
+    cout << "2. Pares" << endl;
+    cout << "3. Impares" << endl;
+    cout << "4. Multiplos de algun numero" << endl;
+    cin >> eleccion;
+    return eleccion;
+}
+
+int main() {
+    
+}
